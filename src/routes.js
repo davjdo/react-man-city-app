611ebdfecc3713a8671bcdd788a61fda4d01b417
davjdo <davido_204@hotmail.com>
2018-10-01 00:26:34 +0200

    add Home, Featured, Stripes and Text components with react-move animations

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
+import Home from './components/Home';
 
 const Routes = props => {
-  return <Layout />;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
