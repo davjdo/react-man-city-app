3a5b34178785fd5b784fb0557039f59864a3675c
davjdo <davido_204@hotmail.com>
2018-10-01 01:46:18 +0200

    add Matches and Tag component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Featured from './Featured';
+import Matches from './Matches';
 
 class Home extends Component {
   render() {
     return (
       <div className="bck_blue">
         <Featured />
+        <Matches />
       </div>
     );
   }
