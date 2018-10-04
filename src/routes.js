e5c8887ce0d26902ad01bca88c79ead8df93457f
davjdo <davido_204@hotmail.com>
2018-10-04 15:25:23 +0200

    add Admin section with Dashboard and AdminNav components and hoc AdminLayout component

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Layout from './hoc/Layout/Layout';
+import Layout from './hoc/Layout';
 import Home from './components/Home';
 import Login from './components/Login';
+import Dashboard from './components/Admin/Dashboard';
 
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={Login} path="/login" />
         <Route exact component={Home} path="/" />
       </Switch>
